7a2fba77ceb82024f9e1f47ed2e170f9535baa15
Sebastian B <sebbarry@comcast.net>
2023-10-25 10:07:21 -0600

    Svelte

---
+++ portfolio/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Home from './components/Home.svelte';
+	import { Route, Router, Link } from "svelte-routing";
+</script>
+
+<Router>
+  <main class="app">
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+    <Route path="/" component={Home} />
+  </main>
+</Router>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		font-family: "Roboto";
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ portfolio/src/components/Home.svelte
@@ -0,0 +1,29 @@
+<script>
+import NameTitle from './NameTitle.svelte';
+</script>
+
+<section id="main__section">
+	<NameTitle />
+	<p>Software Developer</p>
+	<ul>
+		<li>Work</li>
+		<li>About</li>
+		<li>Contact</li>
+		<li>Resume</li>
+	</ul>
+</section>
+
+<style>
+	section {
+		text-align: center;
+		padding: 1em;
+		min-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
